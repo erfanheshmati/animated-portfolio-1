4b80bc0e2605edea58373c6fbbb59054705d5913
Erfan Heshmati <heshmati74@gmail.com>
2024-04-05 15:14:32 +0330

    animated hero section design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./app.scss"
+import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 
 export default function App() {
   return (
     <div>
-      <section id="Homepage"><Navbar /></section>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
       <section id="Services">Parallax</section>
       <section>Services</section>
       <section id="Portfolio">Parallax</section>
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,49 @@
+import "./hero.scss"
+import { motion } from "framer-motion"
+
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    },
+    scrollButton: {
+        opacity: 0,
+        y: 10,
+        transition: {
+            duration: 2,
+            repeat: Infinity,
+        }
+    }
+}
+
+export default function Hero() {
+    return (
+        <div className="hero">
+            <div className="wrapper">
+                <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+                    <motion.h2 variants={textVariants}>Erfan Heshmati</motion.h2>
+                    <motion.h1 variants={textVariants}>Web Developer & UI/UX Designer</motion.h1>
+                    <motion.div className="buttons" variants={textVariants}>
+                        <motion.button variants={textVariants}>See the Latest Works</motion.button>
+                        <motion.button variants={textVariants}>Contact Me</motion.button>
+                    </motion.div>
+                    <motion.img src="/scroll.png" alt="" variants={textVariants} animate="scrollButton" />
+                </motion.div>
+            </div>
+            <div className="slidingTextContainer">
+                Writer Content Creator Influencer
+            </div>
+            <div className="imageContainer">
+                <img src="/hero.png" alt="" />
+            </div>
+        </div>
+    )
+}

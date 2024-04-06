650956c02a3c09c42bf20a6d6b52e0b865cfeaca
Erfan Heshmati <heshmati74@gmail.com>
2024-04-06 13:28:12 +0330

    items on scroll animation

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react"
 import "./portfolio.scss"
-import { motion, useScroll, useSpring } from "framer-motion"
+import { motion, useScroll, useSpring, useTransform } from "framer-motion"
 
 const items = [
     {
@@ -30,9 +30,26 @@ const items = [
 ]
 
 const Single = ({ item }) => {
+    const ref = useRef()
+
+    const { scrollYProgress } = useScroll({ target: ref })
+
+    const y = useTransform(scrollYProgress, [0, 1], [-300, 300])
+
     return (
         <section>
-            {item.title}
+            <div className="container">
+                <div className="wrapper">
+                    <div className="imageContainer" ref={ref}>
+                        <img src={item.img} alt="" />
+                    </div>
+                    <motion.div className="textContainer" style={{ y }}>
+                        <h2>{item.title}</h2>
+                        <p>{item.desc}</p>
+                        <button>See Demo</button>
+                    </motion.div>
+                </div>
+            </div>
         </section>
     )
 }

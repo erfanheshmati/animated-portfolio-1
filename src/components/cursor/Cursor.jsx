d20d423584848df4ba46b768ca798f98f9e07ffa
Erfan Heshmati <heshmati74@gmail.com>
2024-04-06 17:37:51 +0330

    mouse cursor animation

---
+++ src/components/cursor/Cursor.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"
+import "./cursor.scss"
+import { motion } from "framer-motion"
+
+export default function Cursor() {
+    const [position, setPosition] = useState({ x: 0, y: 0 })
+
+    useEffect(() => {
+        const mouseMove = (e) => {
+            setPosition({ x: e.clientX, y: e.clientY })
+        }
+        window.addEventListener("mousemove", mouseMove)
+        return () => {
+            window.removeEventListener("mousemove", mouseMove)
+        }
+    }, [])
+
+    return (
+        <motion.div className="cursor" animate={{ x: position.x, y: position.y }}></motion.div>
+    )
+}
